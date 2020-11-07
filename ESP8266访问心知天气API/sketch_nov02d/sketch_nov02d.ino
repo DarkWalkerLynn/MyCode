f2cece41feee04dd8e4272cc15f33ab316d92d85
DarkWalkerLynn <70731361+DarkWalkerLynn@users.noreply.github.com>
2020-11-07 23:51:25 +0800

    Add files via upload

--- ESP8266访问心知天气API/sketch_nov02d/sketch_nov02d.ino
+++ ESP8266访问心知天气API/sketch_nov02d/sketch_nov02d.ino
@@ -1,71 +1,69 @@
 #include<ESP8266WiFi.h>
 #include<ArduinoJson.h>
 
-#define DebugBegin(baud_rate) Serial.begin(baud_rate)
-#define DebugPrintln(message) Serial.println(message)
-#define DebugPrint(message) Serial.print(message)
-
 const char* ssid ="";//输入热点名称
 const char* password ="";//输入热点密码
 const char* host ="api.seniverse.com";
 const char* APIKEY ="";//输入自己申请的知心天气私钥
-const char* city ="hangzhou";
+const char* city ="hangzhou";//可根据需要改为其余城市的拼音
 const char* language ="zh-Hans";
 
 const unsigned long BAUD_RATE=115200;
 const unsigned long HTTP_TIMEOUT=5000;
 const size_t MAX_CONTENT_SIZE=1000;
 
-struct WeatherData{
+struct WeatherData{//存储天气数据的结构体，可根据需要自行添加成员变量
   char city[16];
   char weather[32];
   char temp[16];
   char udate[32];
 };
 
-WiFiClient client;
+WiFiClient client;//创建了一个网络对象
 char response[MAX_CONTENT_SIZE];
 char endOfHeaders[]="\r\n\r\n";
 
 void setup() {
-  // put your setup code here, to run once:
-  delay(1000);
-  WiFi.mode(WIFI_STA);
-  DebugBegin(BAUD_RATE);
-  DebugPrint("Connecting to");
-  DebugPrintln(ssid);
-  WiFi.begin(ssid,password);
-  WiFi.setAutoConnect(true);
-  while(WiFi.status()!=WL_CONNECTED){
-    delay(500);
-    DebugPrint(".");
-  }
-  DebugPrintln("");
-  DebugPrintln("WiFi connected");
-  delay(500);
-  DebugPrintln("IP address:");
-  DebugPrintln(WiFi.localIP());
-  //client.setTimeout(HTTP_TIMEOUT);
+  Serial.begin(BAUD_RATE);
+  wifiConnect();//连接WiFi
+  client.setTimeout(HTTP_TIMEOUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   while(!client.connected()){
-    if(!client.connect(host,80)){//尝试进行连接
-      DebugPrintln("connection....");
+    if(!client.connect(host,80)){//尝试建立连接
+      Serial.println("connection....");
       delay(500);
     }
   }
-  //连接成功
-  if(sendRequest(host,city,APIKEY)&&skipResponseHeaders()){
-    clrEsp8266ResponseBuffer();
-    readReponseContent(response,sizeof(response));
+  //连接成功，发送GET请求
+  if(sendRequest(host,city,APIKEY)&&skipResponseHeaders()){//发送http请求 并且跳过响应头
+    clrEsp8266ResponseBuffer();//清除缓存
+    readReponseContent(response,sizeof(response));//从HTTP服务器响应中读取正文
     WeatherData weatherData;
-    if(parseUserData(response,&weatherData)){
-      printUserData(&weatherData);
+    if(parseUserData(response,&weatherData)){//判断Json数据包是否分析成功
+      printUserData(&weatherData);//输出读取到的天气信息
     }
   }
+  stopConnect();
   delay(5000);
+}
+
+void wifiConnect(){
+  WiFi.mode(WIFI_STA);//设置esp8266工作模式
+  Serial.print("Connecting to");
+  Serial.println(ssid);
+  WiFi.begin(ssid,password);//连接WiFi
+  WiFi.setAutoConnect(true);
+  while(WiFi.status()!=WL_CONNECTED){//该函数返回WiFi的连接状态
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  delay(500);
+  Serial.println("IP address:");
+  Serial.println(WiFi.localIP());
 }
 
 bool sendRequest(const char* host,const char* cityid,const char* apiKey){
@@ -77,8 +75,8 @@ bool sendRequest(const char* host,const char* cityid,const char* apiKey){
   GetUrl+=language;
   GetUrl+="&unit=c ";
   client.print(String("GET ")+GetUrl+"HTTP/1.1\r\n"+"Host:"+host+"\r\n"+"Connection:close\r\n\r\n");
-  DebugPrintln("creat a request:");
-  DebugPrintln(String("GET ")+GetUrl+"HTTP/1.1\r\n"+"Host:"+host+"\r\n"+"Connection:close\r\n\r\n");
+  Serial.println("creat a request:");
+  Serial.println(String("GET ")+GetUrl+"HTTP/1.1\r\n"+"Host:"+host+"\r\n"+"Connection:close\r\n\r\n");
   delay(1000);
   return true;
 }
@@ -86,7 +84,7 @@ bool sendRequest(const char* host,const char* cityid,const char* apiKey){
 bool skipResponseHeaders(){
   bool ok=client.find(endOfHeaders);
   if(!ok){
-    DebugPrintln("No response of invalid response!");
+    Serial.println("No response of invalid response!");
   }
   return ok;
 }
@@ -94,21 +92,21 @@ bool skipResponseHeaders(){
 void readReponseContent(char* content,size_t maxSize){
   size_t length=client.readBytes(content,maxSize);
   delay(100);
-  DebugPrintln("Get the data from Internet");
+  Serial.println("Get the data from Internet");
   content[length]=0;
-  DebugPrintln(content);
-  DebugPrintln("Read data Over!");
-  client.flush();
+  Serial.println(content);//输出读取到的数据
+  Serial.println("Read data Over!");
+  client.flush();//刷新客户端
 }
 
 bool parseUserData(char* content,struct WeatherData* weatherData){
-  DynamicJsonBuffer jsonBuffer;
-  JsonObject&root=jsonBuffer.parseObject(content);
+  DynamicJsonBuffer jsonBuffer;//创建一个动态缓冲区实例
+  JsonObject&root=jsonBuffer.parseObject(content);//根据需要解析的数据来计算缓冲区的大小
   if(!root.success()){
-    DebugPrintln("JSON parsing failed!");
+    Serial.println("JSON parsing failed!");
     return false;
   }
-
+  //复制数据包中所需的字符串
   strcpy(weatherData->city,root["results"][0]["location"]["name"]);
   strcpy(weatherData->weather,root["results"][0]["now"]["text"]);
   strcpy(weatherData->temp,root["results"][0]["now"]["temperature"]);
@@ -119,21 +117,21 @@ bool parseUserData(char* content,struct WeatherData* weatherData){
 
 
 void printUserData(const struct WeatherData* weatherData){
-  DebugPrintln("Print parsed data:");
-  DebugPrint("City:");
-  DebugPrint(weatherData->city);
-  DebugPrint("  Weather:");
-  DebugPrint(weatherData->weather);
-  DebugPrint("  Temp:");
-  DebugPrint(weatherData->temp);
-  DebugPrint("℃");
-  DebugPrint("  UpdateTime:");
-  DebugPrintln(weatherData->udate);
+  Serial.println("Print parsed data:");
+  Serial.print("City:");
+  Serial.print(weatherData->city);
+  Serial.print("  Weather:");
+  Serial.print(weatherData->weather);
+  Serial.print("  Temp:");
+  Serial.print(weatherData->temp);
+  Serial.print("℃");
+  Serial.print("  UpdateTime:");
+  Serial.println(weatherData->udate);
 }
 
 void stopConnect(){
-  DebugPrintln("Disconnect");
-  client.stop();
+  Serial.println("Disconnect");
+  client.stop();//停止客户端访问
 }
 
 void clrEsp8266ResponseBuffer(void){
